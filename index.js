1b6c7c9a0de53602583144610fd414492cea8ca6
Justin Daniel <justinddaniel@gmail.com>
2017-08-24 23:34:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < 4; i++) {array.push
+    (musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let array = []
+  let i = 0
+  while (i >= 0 & i < facts.length)  {
+    console.log (i++); array.push (facts[i-1] + "!!!")
+  }
+  return array
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  let i = 0
+  do {array.push("I love the Beatles!")}
+  while (n - i > 0) {
+    console.log (i++)
+  }
+}
