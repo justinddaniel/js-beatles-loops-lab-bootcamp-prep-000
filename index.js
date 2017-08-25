9cf6b87af32eb0845c10979bbb43e9f74068960a
Justin Daniel <justinddaniel@gmail.com>
2017-08-25 00:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
   var array = []
   let i = 0
-  if (n < 15 | i = 0) do {array.push("I love the Beatles!"); console.log (i++);}
+  if (n < 15) do {array.push("I love the Beatles!"); console.log (i++);}
   while ((n - i + 1) > 0);
   return array
 }
